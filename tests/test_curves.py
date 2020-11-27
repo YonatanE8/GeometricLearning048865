a90bd2aad33d84cca052fca3e2227e1189ac79c3
Yonatan <yonatan@sibylla.io>
2020-11-27 10:14:00 +0200

    debugged the gradient estimations methods and provided 2 light tests

--- tests/test_curves.py
+++ tests/test_curves.py
@@ -25,18 +25,20 @@ class TestCurve:
 
     def test_grad(self, sin_curve):
         interval = sin_curve.get_interval(start=0, end=(2 * np.pi), n_points=100)
-        _, y_grad = sin_curve.grad(interval)
+        x_grad, y_grad = sin_curve.grad(interval)
 
         cos = np.cos(interval)
 
+        assert pytest.approx(np.sum(np.abs(x_grad - np.ones_like(x_grad))), 0, 1e-6)
         assert pytest.approx(np.sum(np.abs(y_grad - cos)), 0, 1e-6)
 
     def test_grad_sq(self, sin_curve):
         interval = sin_curve.get_interval(start=0, end=(2 * np.pi), n_points=100)
-        _, y_grad = sin_curve.grad_sq(interval)
+        x_grad, y_grad = sin_curve.grad_sq(interval)
 
         sin = -np.sin(interval)
 
+        assert pytest.approx(np.sum(np.abs(x_grad - np.zeros_like(x_grad))), 0, 1e-6)
         assert pytest.approx(np.sum(np.abs(y_grad - sin)), 0, 1e-6)
 
 
